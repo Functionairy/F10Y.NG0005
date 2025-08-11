496af541948e7ffcf95f2b6675e3e3e910372e63
davidcoats <david@getsafetycone.com>
2025-08-10 23:12:47 -0700

    Initial commit.

---
+++ source/F10Y.NG0005.Construction/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace F10Y.NG0005.Construction
+{
+    public static class Instances
+    {
+		
+    }
+}
---
+++ source/F10Y.NG0005/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace F10Y.NG0005
+{
+	/// <summary>
+	/// CliWrap package selector.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
